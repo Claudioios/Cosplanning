d6671fc902a84af407a43e977063bbe0ac140fd8
Claudioios <57898958+Claudioios@users.noreply.github.com>
2022-02-08 09:09:10 +0100

    Start Project

---
+++ Cosplanning/Cosplanning/CosplanningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosplanningApp.swift
+//  Cosplanning
+//
+//  Created by Claudio Silvestri on 08/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CosplanningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
