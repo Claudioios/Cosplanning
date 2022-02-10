1dd46702d19825719ed00566ad94f984dd404d9a
Claudioios <claudiosilvestri48@yahoo.it>
2022-02-10 09:46:23 +0100

    Add calendar

--- Cosplanning/Cosplanning/PlannerView.swift
+++ Cosplanning/Cosplanning/PlannerView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct PlannerView: View {
+    
+    @State private var date = Date()
+
+    let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let startComponents = DateComponents(year: 2021, month: 1, day: 1)
+        let endComponents = DateComponents(year: 2021, month: 12, day: 31, hour: 23, minute: 59, second: 59)
+        return calendar.date(from:startComponents)!
+            ...
+            calendar.date(from:endComponents)!
+    }()
+    
+    
     var body: some View {
-        Text("Today")
-            .padding()
+        VStack{
+            DatePicker(
+                "Select Date",
+                selection: $date,
+                in: dateRange,
+                displayedComponents: [.date]
+            )
+                .datePickerStyle(GraphicalDatePickerStyle())
+                .padding()
+            Text("today")
+                .padding()
+        }
     }
 }
 
