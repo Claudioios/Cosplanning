737febfe9a5f64301d7be45450ac16915daff7d2
Giuseppe Sica <ssxeps@gmail.com>
2022-02-16 11:01:59 +0100

    Finalmente la navigation view

--- Cosplanning/Cosplanning/CosplanningApp.swift
+++ Cosplanning/Cosplanning/CosplanningApp.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 @main
 struct CosplanningApp: App {
+    
+    
+    init() {
+        UINavigationBar.appearance().backgroundColor = UIColor(Color("ViolaBottoneChiaro"))// UTILISSIMO MI HA SALVATO LA VITA
+            UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+            UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().barTintColor = UIColor(Color("ViolaBottoneChiaro"))
+            UINavigationBar.appearance().isTranslucent = true
+           
+           }
+        
     var body: some Scene {
         WindowGroup {
             ContentTabView()
