0a03cb93dfee9e2700e1db7ae24443bbaebc6709
Claudioios <claudiosilvestri48@yahoo.it>
2022-02-17 10:09:41 +0100

    info

--- Cosplanning/Cosplanning/BudgetListView.swift
+++ Cosplanning/Cosplanning/BudgetListView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct BudgetListView: View {
     
     @State private var Money = 500
-    
+    @State private var Positive = true
     var body: some View {
 //        Rectangle()
 //            .frame(width: .infinity, height: 55)
@@ -29,8 +29,22 @@ struct BudgetListView: View {
 //                }
         HStack
         {
-            Text("\(Money) $")
+            if(Positive)
+            {
+            Text("+ \(Money) $")
+                .font(.title)
+                .fontWeight(.bold)
                 .foregroundColor(.green)
+                .padding()
+            }
+            else
+            {
+                Text("- \(Money) $")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.red)
+                    .padding()
+            }
         }
     }
 }
